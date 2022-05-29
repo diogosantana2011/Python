fdb318f331d90219079b349463a227713b95a694
Diogo Santana <diogo.santana@playtech.com>
2022-05-29 13:55:25 +0200

    (TASK): Adding Q2 C, and re-factor of Q2 A & B.

--- TM112/TMA02/Q2b_ds26748.py
+++ TM112/TMA02/Q2b_ds26748.py
@@ -2,26 +2,8 @@
 ## print out the GPA rounded to 2 decimal places
 
 # Input: a list of of whole positive numbers 
-student_results = ['D', 'A', 'B', 'B', 'C', 'D', 'B', 'F', 'W', 'B', 'W', 'C', 'C']
-#['D', 'A', 'B', 'B', 'C', 'D', 'B', 'F', 'W', 'B', 'W', 'C', 'C']
-# test: ['C', 'C', 'A', 'B', 'D', 'F', 'F', 'F', 'W', 'F', 'W', 'F', 'F']
-## in the range of (0, n+1) where n is 4
-student_grades = []
-
-# Sub-problem: calculate grade based on 
-## input from list
-for grade in student_results:
-    if grade == 'A': 
-        student_grades = student_grades + [4]
-    elif grade == 'B':
-         student_grades = student_grades + [3]
-    elif grade == 'C':
-        student_grades = student_grades + [2]
-    elif grade == 'D':
-        student_grades = student_grades + [1]
-    elif grade == 'F':
-        student_grades = student_grades + [0]
-
+student_grades = [1, 4, 3, 3, 2, 1, 3, 0, 3, 2, 2]
+# Test: [2, 2, 4, 3, 1, 0, 0, 0, 0, 0, 0]
 # Sub-problem: compute sum of a list of ints(student_grades)
 ## Average should not be taken from student_results, as it includes 'W' withdrawn
 length = 0
---
+++ TM112/TMA02/Q2c_ds26748.py
@@ -0,0 +1,46 @@
+# Problem: FINAL - calculate and 
+## print out the GPA rounded to 2 decimal places
+
+# Input: a list of grades, postive whole numbers
+student_results = ['D', 'A', 'B', 'B', 'C', 'D', 'B', 'F', 'W', 'B', 'W', 'C', 'C']
+# Test result (expectation): [1, 4, 3, 3, 2, 1, 3, 0, 3, 2, 2]
+# Borderline input: ['W', 'F', 'F', 'F', 'W', 'F', 'F', 'F', 'W', 'F', 'W', 'F', 'F'] 
+# Test result (expectation): [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+# Output: a list of grades
+## transformed to their respective grade value 
+student_grades = []
+
+for grade in student_results:
+    if grade == 'A': 
+        student_grades = student_grades + [4]
+    elif grade == 'B':
+         student_grades = student_grades + [3]
+    elif grade == 'C':
+        student_grades = student_grades + [2]
+    elif grade == 'D':
+        student_grades = student_grades + [1]
+    elif grade == 'F':
+        student_grades = student_grades + [0]
+        
+# Sub-problem: compute sum of a list of ints(student_grades)
+## Average should not be taken from student_results, as it includes 'W' withdrawn
+length = 0
+for item in student_grades:
+    length = length + 1
+
+# DEBUG
+# Should = 12 if student_results = ['D', 'A', 'B', 'B', 'C', 'D', 'B', 'F', 'W', 'B', 'W', 'C', 'C']
+# print(length)
+
+# Sub-problem: compute the sum of the grades
+total = 0
+for number in student_grades:
+    total = total + number
+# DEBUG
+# Should = 24 if student_results = ['D', 'A', 'B', 'B', 'C', 'D', 'B', 'F', 'W', 'B', 'W', 'C', 'C']
+# print(total)
+
+# OUTPUT: compute mean of student_gpa   
+student_gpa = total / length
+print('The student GPA was: ', round(student_gpa, 2))
