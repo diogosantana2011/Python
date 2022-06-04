6a0641e32201e9df395a2b764511d6d162df778c
Diogo Santana <diogo.santana@playtech.com>
2022-06-04 13:57:48 +0200

    (FIX): Fixed new version of GPA calculator.

--- TM112/TMA02/Q2d_ds26748-2.py
+++ TM112/TMA02/Q2d_ds26748-2.py
@@ -3,22 +3,21 @@
 ### the GPA rounded to 2 decimal places
 
 # Input: a list of grades, postive whole numbers
+# grade_marking = {'A': 4, 'B': 3, 'C': 2, 'D': 1, 'F': 0}
+# # student_results = ['D', 'A', 'B', 'B', 'C', 'D', 'B', 'F', 'W', 'B', 'W', 'C', 'C']
+grades = ['D', 'A', 'B', 'B', 'C', 'D', 'B', 'F', 'W', 'B', 'W', 'C', 'C']
 grade_marking = {'A': 4, 'B': 3, 'C': 2, 'D': 1, 'F': 0}
-student_results = ['D', 'A', 'B', 'B', 'C', 'D', 'B', 'F', 'W', 'B', 'W', 'C', 'C']
+# points = 0
+# length = len(grades)
 student_grades = []
-gpa = 0
+points = 0 
 
-total = 0
-for number in student_grades:
-    total = total + number
-
-length = 0
-for item in student_grades:
-    length = length + 1
-    
-while length:
-    result = grade_marking[student_grades] * total
-    gpa += result
-
+for grade in grades:
+    while 'W' in grades:
+        grades.remove('W')
+        print(grades)
+    if grades != []:
+        points += grade_marking[grade]
+    gpa = points / len(grades)
+        
 print('GPA is: ', round(gpa, 2))
-    
