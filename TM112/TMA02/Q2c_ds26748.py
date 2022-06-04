b347011d5697c60ccdd356597d1799ad31e1c6b2
Diogo Santana <diogo.santana@playtech.com>
2022-06-04 12:37:32 +0200

    (TASK): Updates to Q2 (c) and to Q2 (d).

--- TM112/TMA02/Q2c_ds26748.py
+++ TM112/TMA02/Q2c_ds26748.py
@@ -1,9 +1,9 @@
 # Problem: FINAL - calculate and 
 ## print out the GPA rounded to 2 decimal places
 
-# Input: a list of grades, postive whole numbers
-student_results = ['C', 'C', 'A', 'B', 'D', 'F', 'F', 'F', 'W', 'F', 'W', 'F', 'F']
-# ['D', 'A', 'B', 'B', 'C', 'D', 'B', 'F', 'W', 'B', 'W', 'C', 'C']
+# Input: a list of grades, strings representing each grade
+student_results = ['D', 'A', 'B', 'B', 'C', 'D', 'B', 'F', 'W', 'B', 'W', 'C', 'C']
+# Test input - Initial: ['D', 'A', 'B', 'B', 'C', 'D', 'B', 'F', 'W', 'B', 'W', 'C', 'C']
 # Test result (expectation): [1, 4, 3, 3, 2, 1, 3, 0, 3, 2, 2]
 # GPA: 2.18
 
---
+++ TM112/TMA02/Q2d_ds26748.py
@@ -0,0 +1,6 @@
+# Problem: Calculate GPAS: Version 2
+## calculate and print out 
+### the GPA rounded to 2 decimal places
+
+# Input: a list of grades, postive whole numbers
+[1, 4, 3, 3, 2, 1, 3, 0, 3, 2, 2]
