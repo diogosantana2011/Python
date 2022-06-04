67fc3d0138d1ff6de3008b5cc506ddf16ab16f1a
Diogo Santana <diogo.santana@playtech.com>
2022-06-04 13:06:35 +0200

    (TASK): Q2(D) - Version 2 of GPA calculator.

--- TM112/TMA02/Q2d_ds26748.py
+++ TM112/TMA02/Q2d_ds26748.py
@@ -3,4 +3,37 @@
 ### the GPA rounded to 2 decimal places
 
 # Input: a list of grades, postive whole numbers
-[1, 4, 3, 3, 2, 1, 3, 0, 3, 2, 2]
+student_results = ['D', 'A', 'B', 'B', 'C', 'D', 'B', 'F', 'W', 'B', 'W', 'C', 'C']
+student_grades = []
+
+for result in student_results:
+    # Calculate Grade
+    if result == 'A': 
+        student_grades = student_grades + [4]
+    elif result == 'B':
+         student_grades = student_grades + [3]
+    elif result == 'C':
+        student_grades = student_grades + [2]
+    elif result == 'D':
+        student_grades = student_grades + [1]
+    elif result == 'F':
+        student_grades = student_grades + [0]
+    
+    # Calculate GPA: 
+    for grade in student_grades:
+        total = 0
+        for number in student_grades:
+            total = total + number
+
+        length = 0
+        for item in student_grades:
+
+            length = length + 1
+        gpa = total / length 
+
+# print(student_grades)
+# print(student_results)
+# print(total)
+# print(length)
+print('GPA is: ', round(gpa, 2))
+    
