b87b22245867364bd4ee2b9886d41873f9461635
Diogo Santana <diogo.santana@playtech.com>
2022-06-05 08:59:35 +0200

    (TASK): Change to variable on Q2 (d).

--- TM112/TMA02/Q2d_ds26748-2.py
+++ TM112/TMA02/Q2d_ds26748-2.py
@@ -5,14 +5,14 @@
 # Input: a list of grades, postive whole numbers
 grades = ['C', 'C', 'A', 'B', 'D', 'F', 'F', 'F', 'W', 'F', 'W', 'F', 'F']
 grade_marking = {'A': 4, 'B': 3, 'C': 2, 'D': 1, 'F': 0}
-points = 0 
+result = 0 
 
 for grade in grades:
     while 'W' in grades:
         grades.remove('W')
         print(grades)
     if grades != []:
-        points += grade_marking[grade]
-    gpa = points / len(grades)
+        result += grade_marking[grade]
+    gpa = result / len(grades)
         
 print('GPA is: ', round(gpa, 2))
