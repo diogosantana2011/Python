e1dd8968296521ab5261f22074b7747c78fd8d8c
Diogo Santana <diogo.santana@playtech.com>
2022-06-11 13:54:43 +0200

    (TASK): Adding v.1 3-triangles turtle script.

--- TM112/TMA02/three_triangles.py
+++ TM112/TMA02/three_triangles.py
@@ -1,2 +1,39 @@
 # Draw three triangles
 ## using turtle
+
+from turtle import *
+import turtle as ws
+
+numshapes = 3
+start_x_pos = 0
+start_y_pos = 0
+
+# Draw triangle
+for sides in range(0, 3):
+    forward(40)
+    left(120)
+    
+# Move to position for second triangle
+penup()
+forward(100)
+pendown()
+
+# Draw triangle
+for sides in range(0, 3):
+    forward(40)
+    left(120)   
+    
+penup()
+left(180)  
+forward(100)
+right(90)
+forward(100)
+right(90)
+pendown()
+
+# Draw triangle
+for sides in range(0, 3):
+    forward(40)
+    left(120)  
+    
+ws.exitonclick()
