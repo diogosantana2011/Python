171c07a89eab2d739e850b085ea5ce9c6e539ba7
Diogo Santana <diogo.santana@playtech.com>
2022-06-11 14:03:16 +0200

    (TASK): Adding 3-triangles-v2.py.

---
+++ TM112/TMA02/three_triangles-v2.py
@@ -0,0 +1,39 @@
+# Draw three triangles
+## using turtle
+
+from turtle import *
+import turtle as ws
+
+# Input:
+numshapesinput = int(input('Please enter numnber of triangles to draw: '))
+
+# Remove duplicate code:
+## Add draw triangle funct
+def draw_triangle(numshapes):
+    for sides in range(0, numshapes):
+        forward(40)
+        left(120)
+
+# Draw triangle
+draw_triangle(numshapesinput)
+
+# Move to position for second triangle
+penup()
+forward(100)
+pendown()
+
+# Draw triangle
+draw_triangle(numshapesinput)
+
+penup()
+left(180)  
+forward(100)
+right(90)
+forward(100)
+right(90)
+pendown()
+
+# Draw triangle
+draw_triangle(numshapesinput)
+    
+ws.exitonclick()
