357ea02535f0491e1fcbc9b2f4e537d9ed31b4f4
Diogo Santana <diegsan20@gmail.com>
2024-04-04 17:50:48 +0000

    (TASK): optimizing script

--- clickhouse_data/database.py
+++ clickhouse_data/database.py
@@ -6,25 +6,11 @@ try:
 except ImportError as err:
     print('Data base library import error', err)
 
-def client():
-    """
-    Function to connect to client.
-    If variable client = False
-    Connection will fail and provide error 
-    """    
-    try:   
-        connection = Client(
-            host=credentials.host
-        )
-        return connection
+client = Client(host=credentials.host)    
+
+def execute_query(query):
+    try:
+        return client.execute(query)        
     except ConnectionRefusedError as err:
         print('Error \n' + err)
         return
-    
-def execute_query(query):
-    try:
-        db_query = client().execute(query)
-        return db_query
-    except ConnectionRefusedError as err:
-        print('Error \n' + err)
-        return       
