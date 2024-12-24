be7e2d1f549d812a5c5af25b58fa12ff77f1499a
Diogo Santana <58599175+diogosantana2011@users.noreply.github.com>
2024-12-24 12:54:44 +0000

    Create christmas_tree.py

---
+++ test/christmas_tree.py
@@ -0,0 +1,17 @@
+import numpy
+#########################################
+#      Christmas Tree                   #
+#########################################
+
+x = numpy.arange(7, 16)
+y = numpy.arange(1, 18, 2)
+z = numpy.column_stack((x[:: -1], y))
+
+for i, j in z:
+    print(" "*i + '*' *j)
+
+for r in range(3):
+    print(" "*13, ' || ')
+
+print(" "*12, end="\======/")
+print("")
